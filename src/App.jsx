449370721e51afc4efc9f93a2552db57e96e3e83
Ryan Cavalcanti <contatodevryan@gmail.com>
2024-04-27 12:29:35 -0300

    Creating typing field. Adding useState and filters to the input.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import BarraDePesquisa from './components/BarraDePesquisa'
+import Filtro from './components/Filtro'
 import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
-    <Sidebar />
+    <div className='container'>
+      <Sidebar />
+      <div>
+        <BarraDePesquisa />
+        <Filtro />
+      </div>
+    </div>
   )
 }
 
